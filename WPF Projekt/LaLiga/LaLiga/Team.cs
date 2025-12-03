62e6baa3475f712256052b5702bf544f68519209
Torma Alex <torma.alex@students.jedlik.eu>
2025-12-03 13:47:25 +0100

    teszt

---
+++ WPF Projekt/LaLiga/LaLiga/Team.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaLiga
+{
+    public class Team
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Stadium { get; set; }
+        public DateTime FoundationDate { get; set; }
+        public int Titles { get; set; }
+        public bool IsActive { get; set; }
+        public bool HasTitles { get; set; }
+    }
+}
